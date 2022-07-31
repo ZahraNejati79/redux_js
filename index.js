d66c9ed5584a585bb45c7a1994a5f0e5b843f15e
Zahra Nejati <nejatizahra4@gmail.com>
2022-07-31 07:56:44 +0430

    combine reducers

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
 function buyCake() {
   return {
     type: BUY_CAKE,
   };
 }
+function buyIcecream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
 
-const initialState = {
+const initialCakeState = {
   numOfCake: 10,
 };
-const reducer = (state = initialState, action) => {
+const initialIcecreamState = {
+  numOfIcecream: 20,
+};
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return { ...state, numOfCake: state.numOfCake - 1 };
@@ -19,6 +30,18 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+const icecreamReducer = (state = initialIcecreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return { ...state, numOfIcecream: state.numOfIcecream - 1 };
+    default:
+      return state;
+  }
+};
+const reducer = combineReducers({
+  cake: cakeReducer,
+  icecream: icecreamReducer,
+});
 //store
 const store = createStore(reducer);
 
